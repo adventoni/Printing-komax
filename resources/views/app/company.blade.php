78370b7d3bc8c86fb697e8ed8559632e9d49a3d3
Dadan93 <dadan.yulianto@gmail.com>
2018-05-23 10:07:47 +0700

    nambah company komax

--- resources/views/app/company.blade.php
+++ resources/views/app/company.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-<h3>Company</h3>
+<h3>Company Komax</h3>
 <hr>
 <table id="example" class="table table-striped table-bordered" style="width:100%">
         <thead>
